3f806044d9edd08aee380a3dfc0bdd72378b5d00
Violensia28 <graboneid@gmail.com>
2025-09-21 15:43:18 +0700

    Logger: add logRecord()/sdOK() + optional SD logging + ignore backups

--- ci_build/src/logging/Logger.cpp
+++ ci_build/src/logging/Logger.cpp
@@ -1,10 +1,67 @@
 #include "Logger.h"
 #ifdef USE_LOGGING
-void Logger::begin(){ Serial.println("[LOG] start"); }
-void Logger::ensureHeader(){
-  if (!headerPrinted){ Serial.println("[LOG] ------------------------------"); headerPrinted = true; }
+
+// (Opsional) aktifkan logging ke SD dengan -D USE_SDLOG dan tentukan CS pin via -D LOG_SD_CS=5
+#ifdef USE_SDLOG
+  #include <FS.h>
+  #include <SD.h>
+  #ifndef LOG_SD_CS
+  #define LOG_SD_CS 5
+  #endif
+#endif
+
+void Logger::begin() {
+#ifdef USE_SDLOG
+  sd_ok = SD.begin(LOG_SD_CS);
+#else
+  sd_ok = false; // default: tidak menggunakan SD
+#endif
+  Serial.println("[LOG] start");
 }
-void Logger::info(const String& s){ ensureHeader(); Serial.println("[I] " + s); }
-void Logger::warn(const String& s){ ensureHeader(); Serial.println("[W] " + s); }
-void Logger::err (const String& s){ ensureHeader(); Serial.println("[E] " + s); }
+
+void Logger::ensureHeader() {
+  if (!headerPrinted) {
+    Serial.println("[LOG] ------------------------------");
+    headerPrinted = true;
+  }
+}
+
+void Logger::info(const String& s) {
+  ensureHeader();
+  Serial.println("[I] " + s);
+}
+
+void Logger::warn(const String& s) {
+  ensureHeader();
+  Serial.println("[W] " + s);
+}
+
+void Logger::err(const String& s) {
+  ensureHeader();
+  Serial.println("[E] " + s);
+}
+
+void Logger::logRecord(long t_mm, long tgt, long rec, long last, long ratingSel) {
+  ensureHeader();
+  // Tulis ke Serial (aman di semua board)
+  Serial.printf("[REC] t=%ld tgt=%ld rec=%ld last=%ld rating=%ld\n", t_mm, tgt, rec, last, ratingSel);
+
+#ifdef USE_SDLOG
+  if (sd_ok) {
+    File f = SD.open("/motlog.csv", FILE_APPEND);
+    if (f) {
+      // Format CSV sederhana
+      f.printf("%ld,%ld,%ld,%ld,%ld\n", t_mm, tgt, rec, last, ratingSel);
+      f.close();
+    } else {
+      Serial.println("[LOG] SD open fail (/motlog.csv)");
+    }
+  }
+#endif
+}
+
+bool Logger::sdOK() const {
+  return sd_ok;
+}
+
 #endif
--- ci_build/src/logging/Logger.h
+++ ci_build/src/logging/Logger.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifdef USE_LOGGING
 #include <Arduino.h>
+
 class Logger {
 public:
   void begin();
@@ -8,7 +9,15 @@ public:
   void info(const String& s);
   void warn(const String& s);
   void err (const String& s);
+
+  // === API yang diminta SmartUI ===
+  // Rekam satu baris data (tipe dibuat long/int agar fleksibel dengan struktur status kamu)
+  void logRecord(long t_mm, long tgt, long rec, long last, long ratingSel);
+  // Status SD (untuk ikon SD di UI)
+  bool sdOK() const;
+
 private:
   bool headerPrinted = false;
+  bool sd_ok = false;     // default: tidak pakai SD (false)
 };
 #endif
